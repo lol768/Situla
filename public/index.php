2ed30f85a01600fe14e8ddf3ddb981db616c3a69
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-11-18 10:17:49 -0800

    lol screw semicolons

--- public/index.php
+++ public/index.php
@@ -5,11 +5,11 @@
  * LOL SITULA
  */
 
-define('BASE_DIR', dirname(__FILE__) . '/../')
-define('APP_DIR', dirname(__FILE__) . '/../app/')
-define('LIB_DIR', dirname(__FILE__) . '/../app/lib/')
-define('MODELS_DIR', dirname(__FILE__) . '/../app/models/')
-define('CONTROLLERS_DIR', dirname(__FILE__) . '/../app/controllers/')
-define('VIEWS_DIR', dirname(__FILE__) . '/../app/views/')
+define('BASE_DIR', dirname(__FILE__) . '/../');
+define('APP_DIR', dirname(__FILE__) . '/../app/');
+define('LIB_DIR', dirname(__FILE__) . '/../app/lib/');
+define('MODELS_DIR', dirname(__FILE__) . '/../app/models/');
+define('CONTROLLERS_DIR', dirname(__FILE__) . '/../app/controllers/');
+define('VIEWS_DIR', dirname(__FILE__) . '/../app/views/');
 
 require_once(LIB_DIR . 'Bootstrap.php')
